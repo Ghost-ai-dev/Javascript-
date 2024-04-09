9c27e2a4318c5726d6f05ef475ce9661e2dd835e
Ghost-ai-dev <146638577+Ghost-ai-dev@users.noreply.github.com>
2024-04-09 20:22:19 +0530

    Update variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,12 @@
 const accountId = 14338
 let accountEmail ="subhamdas@google.com"
 var accountPassowrd = "67281"
-
+accountCity = "Guwahati"
 console.log(accountId);
+let x
+accountEmail ="dasthemistry@gmail.com"
+console.table([accountId,accountEmail,accountPassowrd,accountCity,x])
+
+/* 
+prefer not to use var in your coding 
+*/
