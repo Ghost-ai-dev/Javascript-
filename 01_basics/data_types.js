42f080f607ff728d98e4c8daa41a09cae7682cae
Ghost-ai-dev <146638577+Ghost-ai-dev@users.noreply.github.com>
2024-04-09 21:03:37 +0530

    Create data_types.js

---
+++ 01_basics/data_types.js
@@ -0,0 +1,15 @@
+/* 
+In javascript there are many datatypes 
+
+those are
+numbers => with digits
+strings => with characters
+boolean => true/false
+null => objects
+undefined => undefined
+
+*/
+let x="Name"
+let y=23
+let z=null
+console.log(typeof z)
