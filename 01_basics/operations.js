2c5c6d8168af7152b53816f558dd5b93574b9d73
Ghost-ai-dev <146638577+Ghost-ai-dev@users.noreply.github.com>
2024-04-11 10:29:16 +0530

    Create operations.js

---
+++ 01_basics/operations.js
@@ -0,0 +1,5 @@
+console.log("1"+3+4); // 134
+console.log(2+3+"1"); //51
+console.log(2+"3"+1); //231
+// This means that Javascript only implements the adding algorithm on the basis of the number at the first 
+console.log(3+3-2/2); 
