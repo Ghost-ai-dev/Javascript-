58b8054262766b76fbdb602ef9b50caf4f075dad
Ghost-ai-dev <146638577+Ghost-ai-dev@users.noreply.github.com>
2024-04-11 09:50:04 +0530

    Create data_type_conversion.js

---
+++ 01_basics/data_type_conversion.js
@@ -0,0 +1,25 @@
+let age = "23abc"
+// console.log(typeof age)
+
+const InNumber = Number(age)
+// console.log(typeof InNumber);
+
+// console.log(InNumber);
+
+let val = null;
+// console.log(typeof val);
+
+const convertingInNum = Number (val);
+// console.log(typeof convertingInNum);
+
+//"33" => 33
+// "33abd" => nan[Not a Number]
+// true = 1; false = 0;
+
+let isloggedIn = "subham";
+let boolInNum = Boolean(isloggedIn);
+// console.log(boolInNum);
+
+let name = 678
+const con = String(name);
+console.log( con);
