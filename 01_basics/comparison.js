2fec2db05873fc7f2c618cc79525265cdc813755
Ghost-ai-dev <146638577+Ghost-ai-dev@users.noreply.github.com>
2024-04-12 12:20:15 +0530

    Create comparison.js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,11 @@
+console.log(2>1);
+console.log(2!=1);
+console.log(2==1);
+
+// this all returns boolean value either true or false
+console.log("1"==1);
+console.log("0"=== 1);
+// those are of different datatypes but also works in the comparison in Javasript
+
+console.log("2" === 2);
+// "===" this signs not only checks the the numbers but also checks the data type of it
